1a2a545ea15cbb928d1c97f3999e9a5d0aaccfd8
iden <dennydharmawan717@gmail.com>
2021-03-19 23:05:14 +0700

    add login with token

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -70,3 +70,66 @@ exports.register = async (req, res) => {
     });
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const schema = Joi.object({
+      email: Joi.string().email().min(10).max(50).required(),
+      password: Joi.string().min(6).max(50).required(),
+    });
+
+    const { error } = schema.validate(req.body);
+
+    if (error)
+      return res.status(400).send({
+        status: "validation failed",
+        message: error.details[0].message,
+      });
+
+    const checkEmail = await users.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!checkEmail)
+      return res.status(400).send({
+        status: "login failed",
+        message: "Your credentials is not valid",
+      });
+
+    const isPassword = await bcrypt.compare(password, checkEmail.password);
+
+    if (!isPassword)
+      return res.status(400).send({
+        status: "login failed",
+        message: "Your credentials is not valid",
+      });
+
+    const secretKey = "kamucantik3492";
+    const token = jwt.sign(
+      {
+        id: checkEmail.id,
+      },
+      secretKey
+    );
+    res.send({
+      status: "success",
+      data: {
+        user: {
+          fullname: checkEmail.fullname,
+          email: checkEmail.email,
+          token,
+        },
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      status: "error",
+      message: "Server Error",
+    });
+  }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 const { getUsers, deleteUser, addUser } = require("../controllers/user");
-const { register } = require("../controllers/auth");
+const { register, login } = require("../controllers/auth");
 
 router.get("/users", getUsers);
 router.delete("/user/:id", deleteUser);
 router.post("/register", register);
-router.post("/user", addUser);
+router.post("/login", login);
 
 module.exports = router;
